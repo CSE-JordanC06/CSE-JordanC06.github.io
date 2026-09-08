03c1a530eee21482e0c6f7d43daee3f23dc41fcd
CSEE-JordanC06 <Chenj24327@gmail.com>
2026-09-08 13:20:49 -0400

    intro.js error

--- csce 242/exercises/04js/scripts/intro.js
+++ csce 242/exercises/04js/scripts/intro.js
@@ -1,5 +1,5 @@
 document.getElementById("btn-show-message").onclick = (e) => {
     document.getElementById("p-message").innerHTML = "Hello World";
     e.currentTarget.innerHTML = "done!";
-};
+}
 
