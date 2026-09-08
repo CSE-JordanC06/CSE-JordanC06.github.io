299fadd5e9d07318126fe90358163f965b4002f3
CSEE-JordanC06 <Chenj24327@gmail.com>
2026-09-08 12:57:21 -0400

    inner.html doenst work

--- csce 242/exercises/04js/scripts/intro.js
+++ csce 242/exercises/04js/scripts/intro.js
@@ -1,1 +1,5 @@
-console.log("Hello, World!");
+document.getElementById("btn-show-message").onclick = (e) => {
+    document.getElementById("p-message").innerHTML = "Hello World";
+    e.currentTarget.innerHTML = "done!";
+};
+
